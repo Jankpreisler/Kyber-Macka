5c80db8339eb5460e3d2140cf61d17d55caceda2
Ján Kristián Preisler <jankpreisler@gmail.com>
2026-04-02 15:03:10 +0200

    pridanie obrazka macky a uprava logikyz projektu

--- main.js
+++ main.js
@@ -8,19 +8,19 @@ const gravitacia = 0.5;
 
 
 const platforms = [
-    { x: 0, y: 380, width: 800, height: 20 },  //zem bordedr
+    { x: 0, y: 380, width: 800, height: 20, color: 'black' },  //zem bordedr
     { x: 0, y: 0, width: 800, height: 1 },  // hore border
     { x: 0, y: 0, width: 1, height: 400 }, // bariera
     { x: 800, y: 0, width: 1, height: 400 }, //bariera
-    { x: 0, y: 335, width:300, height: 400 }, //spawn zone
-    { x: 100, y: 0, width: 100, height: 320 }, //1trupka
-    { x: 200, y: 230, width: 100, height: 90 }, //1.5 trupky hore
-    { x: 200, y: 150, width: 220, height: 20 },
-    { x: 450, y: 270, width: 250, height: 400 }, //2 kanal
-    { x: 430, y: 320, width: 20, height: 350 }, // 1vyko
-    { x: 550, y: 60, width: 150, height: 400 }, // veza 2casti kanalov
-    { x: 530, y: 200, width: 30, height: 200 },
-    { x: 450, y: 60, width: 420, height: 20 },
+    { x: 0, y: 335, width:300, height: 400, color: 'black' }, //spawn zone
+    { x: 100, y: 0, width: 100, height: 320 , color: 'gray' }, //1trupka
+    { x: 200, y: 230, width: 100, height: 90, color: 'gray' }, //1.5 trupky hore
+    { x: 200, y: 150, width: 220, height: 20, color: 'silver' },
+    { x: 450, y: 270, width: 250, height: 400 , color: 'gray'  }, //2 kanal
+    { x: 430, y: 320, width: 20, height: 350, color: 'maroon' }, // 1vyko
+    { x: 550, y: 60, width: 150, height: 400 , color: 'gray' }, // veza 2casti kanalov
+    { x: 530, y: 200, width: 30, height: 200, color: 'gray' },
+    { x: 450, y: 60, width: 420, height: 20 , color: 'silver'},
     
 ];
 
@@ -44,19 +44,38 @@ let player = {
     grounded: false
 };
 
+const macky = {
+    dolava: new Image(),
+    doprava: new Image(),
+    plazeniedoprava: new Image(),
+}
+
+macky.dolava.src = 'asseti/cyber-cat main cahrakter.png';
+macky.doprava.src = 'asseti/Cybermacka druhy pohlad.png';
+macky.plazeniedoprava.src = 'asseti/Plaziaca macka.png';
+
+let actualnaakciacici = macky.doprava;
+
 const keys = { right: false, left: false };
 
 //tutaj mechanika na ovladanie klavesov ked stalece
 window.addEventListener('keydown', (e) => {
-    if (e.key === 'ArrowRight' || e.key === 'd') keys.right = true;
-    if (e.key === 'ArrowLeft' || e.key === 'a') keys.left = true;
+    if (e.key === 'ArrowRight' || e.key === 'd'){
+        keys.right = true;
+        actualnaakciacici = macky.dolava;
+    } 
+    if (e.key === 'ArrowLeft' || e.key === 'a') {
+        keys.left = true;
+        actualnaakciacici = macky.doprava;
+    }
     if ((e.key === 'ArrowUp' || e.key === 'w' || e.key === 'Backspace')  && player.grounded) {
         player.dy = -player.jumpForce;
         player.grounded = false;
     }
     if ((e.key === 'ArrowDown' || e.key === 's') && player.grounded) {
-        player.height = 15;S
+        player.height = 15;
         player.grounded = false;
+        actualnaakciacici = macky.plazeniedoprava
     }
 });
 
@@ -67,6 +86,7 @@ window.addEventListener('keyup', (e) => {
     if ((e.key === 'ArrowDown' || e.key === 's') && player.grounded) {
         player.height = 30;
         player.y -= 15;
+        actualnaakciacici = macky.doprava;
     }
 });
 
@@ -75,15 +95,9 @@ function animovanie() {
     requestAnimationFrame(animovanie);
     c.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (!isobrazokciciNacitani) {
-        c.fillStyle = 'black';
-        c.fillText('Načítavam obrázky...', 10, 20);
-        return; 
-    }
-
     // farbenie platforiem
-    c.fillStyle = '#45a049'; 
     platforms.forEach(platform => {
+        c.fillStyle = platform.color || 'green'; 
         c.fillRect(platform.x, platform.y, platform.width, platform.height);
     });
 
@@ -131,13 +145,13 @@ function animovanie() {
     }   }
     });
 
-    c.drawImage(
-        obrazokcici,    
-        player.x,         
-        player.y,         
-        player.width,     
-        player.height     
-    );
+   if (actualnaakciacici && actualnaakciacici.complete && actualnaakciacici.naturalWidth !== 0) {
+    c.drawImage(actualnaakciacici, player.x, player.y, player.width, player.height);
+}   else {
+    // Ak obrázok chýba alebo sa nenačítal, uvidíš červený štvorec
+    c.fillStyle = 'red';
+    c.fillRect(player.x, player.y, player.width, player.height);
+}
 }
 
 animovanie();
