cf88169a113f95998d5e6fc57fe5184962fe84f6
Ján Kristián Preisler <jankpreisler@gmail.com>
2026-04-11 18:09:44 +0200

    Funkcne nastavenie zvuku

--- MainMenu/menu.js
+++ MainMenu/menu.js
@@ -1,3 +1,19 @@
+const volumeBar = document.getElementById('volumeControl');
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedVolume = localStorage.getItem('gameVolume') || 0.5;
+    if (music) music.volume = savedVolume;
+    if (volumeBar) volumeBar.value = savedVolume;
+});
+
+if (volumeBar) {
+    volumeBar.addEventListener('input', (e) => {
+        const value = e.target.value;
+        if (music) music.volume = value;
+        
+        localStorage.setItem('gameVolume', value);
+    });
+}
 
 function krasneprehadzovanie(url) {
     
@@ -8,3 +24,13 @@ function krasneprehadzovanie(url) {
         window.location.href = url;
     }, 500);
 }
+
+const music = document.getElementById("bgMusic");
+
+window.addEventListener('click', () => {
+    if (music.paused) {
+        music.play().catch(error => {
+            console.log("Autoplay bol zablokovaný, čakám na interakciu.");
+        });
+    }
+}, { once: true }); 
