35eb68ca2d1057302f5f286412a8db06e1addcfb
Ján Kristián Preisler <jankpreisler@gmail.com>
2026-05-25 22:47:42 +0200

    ubdate na hlasitost

--- zvuk.js
+++ zvuk.js
@@ -1,6 +1,12 @@
 window.addEventListener('click', () => {
     const music = document.getElementById('bgMusic');
-    
+    const savedVolume = localStorage.getItem("gameVolume");
+    if (savedVolume !== null) {
+        music.volume = savedVolume;
+    } else {
+        music.volume = 0.5;
+    }
+
     // Skontrolujeme, či už náhodou nehrá, a ak nie, spustíme ju
     if (music.paused) {
         music.play().catch(error => {
