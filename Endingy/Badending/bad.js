098cceb42ee086e1d397859ad7014ec3f2729921
Matej Kapusniak <kapusniakmato@gmail.com>
2026-05-27 16:44:07 +0200

    Endingy

--- Endingy/Badending/bad.js
+++ Endingy/Badending/bad.js
@@ -1,21 +1,31 @@
 const obrazky = [
-            "../../asseti/Cybermacka druhy pohlad.png",
-            "../../asseti/Plaziaca macka.png",
-            "../../asseti/cyber-cat main cahrakter.png",
-            "../../asseti/rokwel.png"
-        ];
-        let aktualnyIndex = 0;
+    "../../asseti/bad_1.png",   
+    "../../asseti/bad_2.png",           
+    "../../asseti/bad_3.png",  
+    "../../asseti/bad_4.png" 
+                    
+];
 
-        const imgElement = document.getElementById('fullscreen-image');
+let aktualnyIndex = 0;
+const imgElement = document.getElementById('fullscreen-image');
 
-        imgElement.addEventListener('click', function () {
-            aktualnyIndex = (aktualnyIndex + 1) % obrazky.length;
-            imgElement.src = obrazky[aktualnyIndex];
-            console.log(aktualnyIndex);
+imgElement.src = obrazky[aktualnyIndex];
+imgElement.style.opacity = 1;
 
-            if (aktualnyIndex === 3) {
+function prepniObrazok() {
+    imgElement.style.opacity = 0;
+
+    setTimeout(() => {
+        aktualnyIndex = (aktualnyIndex + 1) % obrazky.length;
+        imgElement.src = obrazky[aktualnyIndex];
+        imgElement.style.opacity = 1;
+
+        if (aktualnyIndex === obrazky.length - 1) {
+            setTimeout(() => {
                 window.location.href = "/Endingy/Credits/Credits.html";
-                console.log("Som tu");
+            }, 3000);
+        }
+    }, 2000);
+}
 
-            }
-        });
+setInterval(prepniObrazok, 5000);
