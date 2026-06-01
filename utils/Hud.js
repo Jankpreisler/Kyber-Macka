893810310704e2eb7361210f4283671113351ead
Ján Kristián Preisler <jankpreisler@gmail.com>
2026-06-01 18:55:02 +0200

    uz pojdes

--- utils/Hud.js
+++ utils/Hud.js
@@ -1,12 +1,12 @@
 
 const abilityImg = new Image();
-abilityImg.src = "../../asseti/HUD1abilitka.png";
+abilityImg.src = "../../Asseti/HUD1abilitka.png";
 
 const ability2Img = new Image();
-ability2Img.src = "../../asseti/HUD2abilitky.png";
+ability2Img.src = "../../Asseti/HUD2abilitky.png";
 
 const ability3Img = new Image();
-ability3Img.src = "../../asseti/HUD3abilitky.png";
+ability3Img.src = "../../Asseti/HUD3abilitky.png";
 
 const ability4Img = new Image();
-ability4Img.src = "../../asseti/HUDcomplete.png";
+ability4Img.src = "../../Asseti/HUDcomplete.png";
