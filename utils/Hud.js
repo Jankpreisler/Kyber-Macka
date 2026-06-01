f9705b09d94ca00889cf77d2c6a3641c89a1d449
Ján Kristián Preisler <jankpreisler@gmail.com>
2026-06-01 18:43:44 +0200

    Opravenie hudu

--- utils/Hud.js
+++ utils/Hud.js
@@ -1,12 +1,12 @@
 
 const abilityImg = new Image();
-abilityImg.src = "/asseti/HUD1abilitka.png";
+abilityImg.src = "../asseti/HUD1abilitka.png";
 
 const ability2Img = new Image();
-ability2Img.src = "/asseti/HUD2abilitky.png";
+ability2Img.src = "../asseti/HUD2abilitky.png";
 
 const ability3Img = new Image();
-ability3Img.src = "/asseti/HUD3abilitky.png";
+ability3Img.src = "../asseti/HUD3abilitky.png";
 
 const ability4Img = new Image();
-ability4Img.src = "/asseti/HUDcomplete.png";
+ability4Img.src = "../asseti/HUDcomplete.png";
